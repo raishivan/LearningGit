52013b5b76655fafcf52e12b74c103098d3c8c11
raishivan <shivanand.rai@impressico.com>
2021-09-28 15:33:47 +0530

    commiting for conflict

--- FirstTut.java
+++ FirstTut.java
@@ -3,7 +3,7 @@ package coreJava;
 public class FirstTut {
 
 	public static void main(String[] args) {
-		System.out.println("This is the first line cui edit");
+		System.out.println("This is the first line cui edit and this is modified");
 
 	}
 
